a4060fae17c6749634d1ff29d19aef0b62e90996
IrynaRomanych <106173463+IrynaRomanych@users.noreply.github.com>
2022-10-17 18:31:00 +0300

    updated task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,21 @@
 const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
-const list = document.querySelector("#ingredients");
 
-const ingredientsTag = document.createElement("li");
+const list = document.querySelector('#ingredients');
 
-ingredientsTag.classList.add("items");
+const newItems = [];
 
-const newItems = ingredients
-  .map((ingredient) => `<li class="items">${ingredient}</li>`)
-  .join("");
-list.innerHTML = newItems;
+for (let i = 0; i < ingredients.length; i += 1) {
+    const ingredient = ingredients[i];
+    const ingredientsTag = document.createElement('li');
+    ingredientsTag.classList.add('item');
+    ingredientsTag.textContent = ingredient;
+    newItems.push(ingredientsTag);
+}
+list.append(...newItems);
